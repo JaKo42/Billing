fa3e23261f33f00b06bd3f80f91c72cd2cf8f0c0
JaKo42 <kokkinos.Joannis@gmail.com>
2017-12-06 15:47:01 +0100

    Add features

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,62 +1,102 @@
 
 import java.sql.*;
 
-
 public class DBConnection {
 
-    public void estConnection() throws SQLException{
-
-
+    public void initDatabase() throws SQLException {
 
         Connection conn = DriverManager.getConnection("jdbc:sqlite:Artikel.db");
         Statement stat = conn.createStatement();
+        System.out.println("Initialize Database...");
         stat.execute("create table if NOT exists article(name, price);");
         PreparedStatement insArticle = conn.prepareStatement("insert into article values (?, ?);");
+        ResultSet firstInitCheck = stat.executeQuery("select name from article");
 
+
+        if (!firstInitCheck.next()) {
             insArticle.setString(1, "Käse");
-            insArticle.setDouble(2,2.99 );
+            insArticle.setDouble(2, 2.99);
             insArticle.addBatch();
 
             insArticle.setString(1, "Zucchini");
-            insArticle.setDouble(2,0.89 );
+            insArticle.setDouble(2, 0.89);
             insArticle.addBatch();
 
             insArticle.setString(1, "Tomate KG");
-            insArticle.setDouble(2,0.99 );
+            insArticle.setDouble(2, 0.99);
             insArticle.addBatch();
 
             insArticle.setString(1, "Salami");
-            insArticle.setDouble(2,3.00 );
+            insArticle.setDouble(2, 3.00);
             insArticle.addBatch();
 
             insArticle.setString(1, "Energy Drink");
-            insArticle.setDouble(2,0.69 );
+            insArticle.setDouble(2, 0.69);
             insArticle.addBatch();
 
             insArticle.setString(1, "Karotten");
-            insArticle.setDouble(2,1.99 );
+            insArticle.setDouble(2, 1.99);
             insArticle.addBatch();
 
-//            TODO: Auf dublikate prüfen
-            ResultSet dublicateProof = stat.executeQuery("select name from article");
 
-            if ()
-                    {
             conn.setAutoCommit(false);
             insArticle.executeBatch();
             conn.setAutoCommit(true);
-        }
+        } else System.out.println("Database already set!");
+        conn.close();
+    }
+
+    public void printArticle() throws SQLException{
+
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:Artikel.db");
+        Statement stat = conn.createStatement();
 
         ResultSet selectArticle = stat.executeQuery("select * from article;");
             while (selectArticle.next()){
                 System.out.println("Artikel= " + selectArticle.getString("name"));
                 System.out.println("Preis= " + selectArticle.getDouble("price")+" €");
+
             }
 
-            stat.execute("drop table article;");
+
             selectArticle.close();
             conn.close();
 
     }
+//            TODO: mehrere auf einmal hinzufügen Varargs? Arrays?...
+    public void insertArticle(String name, double price) throws SQLException{
 
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:Artikel.db");
+        Statement stat = conn.createStatement();
+
+        PreparedStatement insArt = conn.prepareStatement("insert into article values (?, ?);");
+
+        insArt.setString(1,name);
+        insArt.setDouble(2,price);
+        insArt.execute();
+        System.out.println("Artikel wurde angelegt!");
+
+        conn.close();
+
+    }
+
+    public void removeArticle (String name) throws SQLException{
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:Artikel.db");
+        Statement stat = conn.createStatement();
+
+        PreparedStatement remArt = conn.prepareStatement("DELETE FROM article WHERE name= ?;");
+        remArt.setString(1, name);
+        remArt.execute();
+
+
+
+
+        conn.close();
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,44 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public ArrayList<Artikel> shoppingList = new ArrayList<>();
 
 
 
     public static void main(String[] args){
 
 
-        Scanner codeScanner = new Scanner(System.in);
+        Scanner insert = new Scanner(System.in);
 
         System.out.println("Belegleser Version 0.1");
         System.out.println("start...");
 
-        try {
-            TimeUnit.SECONDS.sleep(4);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            TimeUnit.SECONDS.sleep(4);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
         DBConnection conn = new DBConnection();
         try {
-            conn.estConnection();
+            conn.initDatabase();
+            conn.printArticle();
+            System.out.println("Möchten Sie einen neuen Artikel eingeben?");
+            String answer = insert.next();
+            switch(answer) {
+                case "ja":
+                    System.out.println("Geben Sie Name und Preis ein: ");
+                    conn.insertArticle(insert.next(), insert.nextDouble());
+                    break;
+                case "nein":
+                    break;
+            }
+            System.out.println("Möchten Sie einen Artikel löschen?");
+            answer = insert.next();
+            if (answer.equals("ja")) {
+                System.out.println("Geben Sie Name und Preis ein: ");
+                conn.removeArticle(insert.next());
+                conn.printArticle();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
