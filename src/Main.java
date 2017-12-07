0d6e1e5da0a72a62ca203967ee67cd5a4af02f39
JaKo42 <kokkinos.Joannis@gmail.com>
2017-12-07 16:05:48 +0100

    Add Javafx

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 
 
-public class Main {
+public class Main extends Application {
+
+    private static DBConnection conn = new DBConnection();
 
 
-
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        launch(args);
 
         Scanner insert = new Scanner(System.in);
 
@@ -24,13 +29,13 @@ public class Main {
 //            e.printStackTrace();
 //        }
 
-        DBConnection conn = new DBConnection();
+
         try {
             conn.initDatabase();
             conn.printArticle();
             System.out.println("Möchten Sie einen neuen Artikel eingeben?");
             String answer = insert.next();
-            switch(answer) {
+            switch (answer) {
                 case "ja":
                     System.out.println("Geben Sie Name und Preis ein: ");
                     conn.insertArticle(insert.next(), insert.nextDouble());
@@ -51,4 +56,27 @@ public class Main {
 
 
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        GridPane grid = new GridPane();
+        grid.setHgap(5);
+        grid.setVgap(5);
+        grid.setPadding(new Insets(10,10,10,10));
+
+        Scene mainScene = new Scene(grid, 400, 500);
+
+        primaryStage.setTitle("Artikelstamm");
+        primaryStage.setScene(mainScene);
+
+        Label greeting = new Label("Artikelstamm");
+        grid.add(greeting,1,1,1,1);
+
+
+        primaryStage.show();
+    }
+
+
+
 }
